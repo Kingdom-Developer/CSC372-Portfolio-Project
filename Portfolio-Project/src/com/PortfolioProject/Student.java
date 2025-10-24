1679b446fb516a1ce4e6b01104aa9b26d7c5fc0f
Nolan Hill <nolanhill-developer@pm.me>
2025-10-24 12:07:15 -0700

    Built student class with prviate attributes, constructor and getter/setter methods.

--- Portfolio-Project/src/com/PortfolioProject/Student.java
+++ Portfolio-Project/src/com/PortfolioProject/Student.java
@@ -1,4 +1,44 @@
 package com.PortfolioProject;
 
+// Class to hold student information
 public class Student {
+    // Declare private attributes
+    private String name;
+    private String address;
+    private double Gpa;
+
+    // Parameterized constructor
+    public Student(String name, String address, double Gpa) {
+        // Initialize private attributes
+        this.name = name;
+        this.address = address;
+        this.Gpa = Gpa;
+    }
+
+    // Getter and setter methods for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and setter methods for address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Getter and setter methods for GPA
+    public double getGpa() {
+        return Gpa;
+    }
+
+    public void setGpa(double gpa) {
+        Gpa = gpa;
+    }
 }
